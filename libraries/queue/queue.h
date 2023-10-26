c7298fbf494eb10f6fdced952af8a2e40bca1e68
Renis Myumyun <renismyumyun@gmail.com>
2023-10-26 10:41:53 +0300

    [Queue] Queue is now templated.

--- libraries/queue/queue.h
+++ libraries/queue/queue.h
@@ -1,28 +1,81 @@
+#include <stdexcept>
+
 namespace rsm
 {
 
+template <typename T>
 class queue
 {
 public:
     class Node
     {
     public:
-        Node(int value) : value(value){}
+        Node(T value) : value(value){}
 
-        int value;
+        T value;
         Node* nextNode = nullptr;
     };
 
 public:
-    queue(int value);
+    queue(T value)
+    {
+        Node* newNode = new Node(value);
+        m_first = newNode;
+        m_last = newNode;
+        m_length = 1;
+    }
 
-    void enqueue(int value);
-    void dequeue();
+    void enqueue(T value)
+    {
+        Node* newNode = new Node(value);
+        if(m_length == 0)
+        {
+            m_first = newNode;
+            m_last = newNode;
+        }
+        else
+        {
+            m_last->nextNode = newNode;
+            m_last = newNode;
+        }
 
-    auto get_first() -> Node*;
-    auto get_last() -> Node*;
+        m_length++;
+    }
 
-    auto get_length() -> const int;
+    void dequeue()
+    {
+        if(m_length == 0)
+        {
+            throw std::invalid_argument("No elements to dequeue");
+        }
+        if(m_length == 1)
+        {
+            m_first = nullptr;
+            m_last = nullptr;
+            return;
+        }
+
+        Node* firstNode = get_first();
+        m_first = firstNode->nextNode;
+        delete firstNode;
+
+        m_length--;
+    }
+
+    auto get_first() -> Node*
+    {
+        return m_first;
+    }
+
+    auto get_last() -> Node*
+    {
+        return m_last;
+    }
+
+    auto get_length() -> const int
+    {
+        return m_length;
+    }
 
 private:
     Node* m_first;

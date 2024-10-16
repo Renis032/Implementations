e0af20e6c7e500ff1a21e78cafd9bb0c17b27445
Renis Myumyun <renismyumyun@gmail.com>
2024-10-16 12:26:57 +0300

    Cleared main.cpp

--- main.cpp
+++ main.cpp
@@ -1,18 +1,7 @@
 #include <iostream>
-#include "libraries/DataStructures/priority_queue/priority_queue.h"
 
 int main()
 {
-    rsm::priority_queue<int> queue(5, 7);
-    queue.push(1, 13);
-    queue.push(11, 1);
-    queue.push(19, 9);
-    queue.print_queue();
-
-    std::cout << "--------" << std::endl;
-
-    queue.dequeue();
-    queue.print_queue();
 
 
     return 0;

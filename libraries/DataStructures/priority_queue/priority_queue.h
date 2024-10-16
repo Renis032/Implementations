88b16c5b1a6258b219503185ec79d0d5a8228388
Renis Myumyun <renismyumyun@gmail.com>
2024-10-16 11:44:40 +0300

    [MaxPriorityQueue] - initial push function.

---
+++ libraries/DataStructures/priority_queue/priority_queue.h
@@ -0,0 +1,81 @@
+#pragma once
+
+#include "../linked_list/linked_list.h"
+#include <iostream>
+
+namespace rsm
+{
+
+template <typename T>
+class priority_queue
+{
+private:
+struct Node
+{
+    Node(T value, int priority) : value(value), 
+                                  priority(priority){}
+
+    T value;
+    int priority;
+    Node* nextNode = nullptr;
+};
+
+public:
+    priority_queue() : m_head(nullptr), m_tail(nullptr), m_length(0) {}
+    priority_queue(T value, int priority)
+    {
+        Node* node = new Node(value, priority);
+        node->priority = priority;
+        m_head = node;
+        m_tail = node;
+        m_length = 1;
+    }
+
+    void push(T value, int priority)
+    {
+        Node* newNode = new Node(value, priority);
+        newNode->priority = priority;
+        if(m_head == nullptr)
+        {
+            m_head = newNode;
+            m_tail = newNode;
+        }
+        else if(m_head->priority < priority)
+        {
+            newNode->nextNode = m_head;
+            m_head = newNode;
+        }
+        else
+        {
+            Node* temp = m_head;
+            while(temp->nextNode != nullptr &&
+                  temp->nextNode->priority >= newNode->priority)
+            {
+                temp = temp->nextNode;
+            }
+
+            newNode->nextNode = temp->nextNode;
+            temp->nextNode = newNode;
+        }
+
+        m_length++;
+    }
+
+    void print_queue()
+    {
+        Node* temp = m_head;
+        while (temp)
+        {
+            std::cout << "Value: " << temp->value << " Priority: " << temp->priority << std::endl;
+            temp = temp->nextNode;
+        }
+    }
+
+private:
+    Node* m_head = nullptr;
+    Node* m_tail = nullptr;
+    int m_length = 0;
+};
+
+} // end of namespace rsm
+
--- main.cpp
+++ main.cpp
@@ -1,23 +1,14 @@
 #include <iostream>
-#include "libraries/DataStructures/binary_tree/binary_tree.h"
+#include "libraries/DataStructures/priority_queue/priority_queue.h"
 
 int main()
 {
-    rsm::binary_tree<int> tree;
-    tree.insert(5);
-    tree.insert(3);
-    tree.insert(2);
-    tree.insert(17);
-    tree.insert(10);
-    tree.insert(8);
+    rsm::priority_queue<int> queue(5, 7);
+    queue.push(1, 11);
+    queue.push(11, 11);
+    queue.push(19, 11);
+    queue.print_queue();
 
-    tree.printBFS(tree.get_root());
 
-    std::cout << std::endl;
-    std::cout << std::endl;
-    std::cout << std::endl;
-    tree.invertTree(tree.get_root());
-    tree.printBFS(tree.get_root());
-    
     return 0;
 }

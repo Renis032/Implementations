9c2c7fb80953e59f9b3d61ed5d2f059331edd064
Renis Myumyun <renismyumyun@gmail.com>
2024-10-29 13:47:20 +0200

    Revert "[PriorityQueue] - Changed implementation to use rsm::vector like heap."
    This reverts commit a2b1b8c3b6733f9e46478215a275201980c7d044.

--- libraries/DataStructures/priority_queue/priority_queue.h
+++ libraries/DataStructures/priority_queue/priority_queue.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include "../vector/vector.h"
+#include "../linked_list/linked_list.h"
 #include <iostream>
 
 namespace rsm
@@ -9,104 +9,115 @@ namespace rsm
 template <typename T>
 class priority_queue
 {
+private:
+struct Node
+{
+    Node(T value, int priority) : value(value), 
+                                  priority(priority){}
+
+    T value;
+    int priority;
+    Node* nextNode = nullptr;
+};
+
 public:
-    auto empty() const -> const bool
+    priority_queue() : m_head(nullptr), m_tail(nullptr), m_length(0) {}
+    priority_queue(T value, int priority)
     {
-        return m_heap.empty();
+        Node* node = new Node(value, priority);
+        node->priority = priority;
+        m_head = node;
+        m_tail = node;
+        m_length = 1;
     }
 
-    void push(const T& value)
+    void enqueue(T value, int priority)
     {
-        m_heap.push_back(value);
-        heapifyUp(m_heap.size() - 1);
-    }
-
-    auto top() -> const T&
-    {
-        if(empty())
+        Node* newNode = new Node(value, priority);
+        newNode->priority = priority;
+        if(m_head == nullptr)
         {
-            std::out_of_range("Priority Queue is empty");
+            m_head = newNode;
+            m_tail = newNode;
+        }
+        else if(m_head->priority < priority)
+        {
+            newNode->nextNode = m_head;
+            m_head = newNode;
+        }
+        else
+        {
+            Node* temp = m_head;
+            while(temp->nextNode != nullptr &&
+                  temp->nextNode->priority >= newNode->priority)
+            {
+                temp = temp->nextNode;
+            }
+
+            newNode->nextNode = temp->nextNode;
+            temp->nextNode = newNode;
         }
 
-        return m_heap.front();
+        m_length++;
     }
 
-    void pop()
+    void dequeue()
     {
-        if(empty())
+        if(get_length() == 0)
         {
-            std::out_of_range("Priority Queue is empty");
+            return;
         }
 
-        m_heap[0] = m_heap.back();
-        m_heap.pop_back();
-        if(false == empty())
+        if(get_length() == 1)
         {
-            heapifyDown(0);
+            m_head = nullptr;
+            m_tail = nullptr;
+            m_length--;
+            return;
+        }
+
+        Node* firstNode = get_head();
+        m_head = firstNode->nextNode;
+        delete firstNode;
+
+        m_length--;
+    }
+
+    void print_queue()
+    {
+        Node* temp = m_head;
+        while(temp)
+        {
+            std::cout << "Value: " << temp->value << " Priority: " << temp->priority << std::endl;
+            temp = temp->nextNode;
         }
     }
 
-    auto size() const -> const size_t
+    
+    bool empty()
     {
-        return m_heap.size();
+        return m_length == 0;
+    }
+
+    auto get_head() -> Node*
+    {
+        return m_head;
+    }
+
+    auto get_tail() -> Node*
+    {
+        return m_tail;
+    }
+
+    auto get_length() -> const int
+    {
+        return m_length;
     }
 
 private:
-    auto parent(const int idx) -> int
-    {
-        return (idx - 1) / 2;
-    }
-
-    auto leftChild(const int idx) -> int
-    {
-        return 2 * idx + 1;
-    }
-
-    auto rightChild(const int idx) -> int
-    {
-        return 2 * idx + 2;
-    }
-
-    void heapifyUp(int idx)
-    {
-        while (idx > 0 && m_heap[parent(idx)] < m_heap[idx])
-        {
-            auto temp = m_heap[parent(idx)];
-            m_heap[parent(idx)] = m_heap[idx];
-            m_heap[idx] = temp;
-            idx = parent(idx);
-        }
-    }
-
-    void heapifyDown(int idx)
-    {
-        int size = m_heap.size();
-        int largest = idx;
-        int left = leftChild(idx);
-        int right = rightChild(idx);
-
-        if(left < size && m_heap[left] > m_heap[largest])
-        {
-            largest = left;
-        }
-
-        if(right < size && m_heap[right] > m_heap[largest])
-        {
-            largest = right;
-        }
-        
-        if(largest != idx)
-        {
-            auto temp = m_heap[largest];
-            m_heap[largest] = m_heap[idx];
-            m_heap[idx] = temp;
-
-            heapifyDown(largest);
-        }
-    }
-
-private:
-    rsm::vector<T> m_heap;
+    Node* m_head = nullptr;
+    Node* m_tail = nullptr;
+    int m_length = 0;
 };
 
 } // end of namespace rsm
